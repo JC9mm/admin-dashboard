c63e1df8252c9b0ca3abe53d574269decf728587
Jorge Castillo <65986285+JC9mm@users.noreply.github.com>
2025-03-30 02:27:22 -0400

    Update `ProductsPage` and `searchAction` functions
    * **ProductsPage**: Add `searchParams` parameter, remove `props` parameter, and update `getProducts` function call to use `searchParams.q` and `searchParams.offset`.
    * **searchAction**: Use `router.push` instead of `router.replace` and add a `scroll: false` option to the `router.push` call.

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -5,11 +5,8 @@ import { ProductsTable } from './products-table';
 import { getProducts } from '@/lib/db';
 
 export default async function ProductsPage(
-  props: {
-    searchParams: Promise<{ q: string; offset: string }>;
-  }
+  searchParams: { q: string; offset: string }
 ) {
-  const searchParams = await props.searchParams;
   const search = searchParams.q ?? '';
   const offset = searchParams.offset ?? 0;
   const { products, newOffset, totalProducts } = await getProducts(
